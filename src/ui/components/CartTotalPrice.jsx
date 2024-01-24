354c9d058333d44a0b61650c0020189e90bb6772
Filipe Mai <f.nunesmai19@gmail.com>
2024-01-24 11:33:05 -0300

    Provador virtual criado

--- src/ui/components/CartTotalPrice.jsx
+++ src/ui/components/CartTotalPrice.jsx
@@ -1,24 +1,29 @@
-// CartTotalPrice.js
 import PropTypes from "prop-types";
 import React from "react";
 import "../styles/cart-total-price.css";
 
 const CartTotalPrice = ({ cartItems }) => {
-    const cartItemsValue = cartItems.reduce((total, item) => total + item.preco*item.quantidade, 0);
+  const cartItemsValue = cartItems.reduce(
+    (total, item) => total + item.preco * item.quantidade,
+    0
+  );
 
-    return (
-        <div className="cart-price-container">
-            <h1>Total</h1>
-            <p>Sub-total: R$ {cartItemsValue.toFixed(2)}</p>
-            <p>Frete: R$ 50.00</p>
-            <p>Total: R$ {(cartItemsValue + 50).toFixed(2)}</p>
-            <button>Finalizar</button>
-        </div>
-    );
-}
+  const shippingCost = 50.0;
+  const totalPrice = cartItemsValue + shippingCost;
+
+  return (
+    <div className="cart-price-container">
+      <h1>Total</h1>
+      <p>Sub-total: R$ {cartItemsValue.toFixed(2)}</p>
+      <p>Frete: R$ {shippingCost.toFixed(2)}</p>
+      <p>Total: R$ {totalPrice.toFixed(2)}</p>
+      <button>Finalizar</button>
+    </div>
+  );
+};
 
 CartTotalPrice.propTypes = {
-    cartItems: PropTypes.array.isRequired,
+  cartItems: PropTypes.array.isRequired,
 };
 
 export default CartTotalPrice;
